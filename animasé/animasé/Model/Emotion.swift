4a63a68fc96d69292614ad2adc22e7c2f772f6a8
inxudianz <43067068+inxudianz@users.noreply.github.com>
2019-05-14 07:45:08 +0700

    Adding Files

---
+++ animasé/animasé/Model/Emotion.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  animasé
+//
+//  Created by William Inx on 14/05/19.
+//  Copyright © 2019 William Inx. All rights reserved.
+//
+
+import Foundation
---
+++ animasé/animasé/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animasé
+//
+//  Created by William Inx on 14/05/19.
+//  Copyright © 2019 William Inx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
