028a4b59c12baebf333cdf94f099be8cd39b093c
inxudianz <william.inxcorp@gmail.com>
2019-05-16 07:21:21 +0700

    Added base layout

---
+++ animasé/animasé/Model/Face.swift
@@ -0,0 +1,44 @@
+//
+//  Face.swift
+//  animasé
+//
+//  Created by William Inx on 14/05/19.
+//  Copyright © 2019 William Inx. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Face{
+    
+    // Init Head
+    var headPath = UIBezierPath(arcCenter: CGPoint(x: 0, y: 25), radius: CGFloat(80), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true)
+    var head = CAShapeLayer()
+    var eyePath = UIBezierPath(arcCenter: CGPoint(x: 0, y: 0), radius: CGFloat(12), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true)
+    var lEye = CAShapeLayer()
+    
+    var rEye = CAShapeLayer()
+    
+    var mouthPath = UIBezierPath(arcCenter: CGPoint(x: 0, y: 25), radius: CGFloat(35), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi), clockwise: true)
+    var mouth = CAShapeLayer()
+    
+    init() {
+        head.fillColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+        head.path = headPath.cgPath
+        
+        lEye.fillColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        lEye.path = eyePath.cgPath
+        lEye.position.x = -10
+        
+        
+        rEye.fillColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        rEye.path = eyePath.cgPath
+        rEye.position.x = 35
+        
+        mouth.path = mouthPath.cgPath
+        mouth.fillColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        mouth.position.x = 10
+        mouth.position.y = 15
+    }
+
+}
--- animasé/animasé/View/MainViewViewController.swift
+++ animasé/animasé/View/MainViewViewController.swift
@@ -10,20 +10,50 @@ import UIKit
 
 class MainViewViewController: UIViewController {
     
-    var foo :UIView!
+    // Init objects
+    var landView :UIView!
+    var skyView :UIView!
+    
+    var faceView :UIView!
+    var boyFace :Face!
+    
+    var but :UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        foo = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        foo.backgroundColor = .blue
         
-        self.view.addSubview(foo)
+        // Init landView
+        landView = UIView(frame: CGRect(x: 0, y: self.view.frame.maxY - (self.view.frame.maxY / 3), width: self.view.frame.maxX, height: self.view.frame.maxY / 3))
+        landView.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         
+        // Init skyView
+        skyView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.maxX, height: self.view.frame.maxY))
+        skyView.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        
+        // Init faceView
+        faceView = UIView(frame: CGRect(x: self.view.frame.maxX / 3, y: (skyView.frame.maxY / 2), width: self.view.frame.maxX / 3, height: self.view.frame.maxY / 5))
+        boyFace = Face()
+
+        
+        but = UIButton(type: .system)
+        but.frame = CGRect(x: 10, y: 10, width: 100, height: 100)
+        but.setTitle("ASD", for: .normal)
+        
+        but.addTarget(self, action: #selector(ger), for: .touchDown)
+        // add views to main viewcontroller
+        self.view.addSubview(skyView)
+        self.view.addSubview(landView)
+        self.view.addSubview(faceView)
+        faceView.layer.addSublayer(boyFace.head)
+        faceView.layer.addSublayer(boyFace.lEye)
+        faceView.layer.addSublayer(boyFace.rEye)
+        faceView.layer.addSublayer(boyFace.mouth)
+        self.view.addSubview(but)
+
         
         // Do any additional setup after loading the view.
     }
-    
 
     /*
     // MARK: - Navigation
@@ -34,5 +64,16 @@ class MainViewViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    @objc func ger() {
+        animHead()
+    }
+    func animHead() {
+        UIView.animate(withDuration: 3) {
+            self.boyFace.head.position = CGPoint(x: 20, y: 20)
+        }
+        
+        }
+    
 }
+
